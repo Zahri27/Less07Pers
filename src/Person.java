457dfd5f3a382c6eefc5ebe3b79c2a1b50ba967e
Zahri27 <privetmurik@gmail.com>
2024-03-17 14:42:57 +0200

    amendment

--- src/Person.java
+++ src/Person.java
@@ -4,24 +4,26 @@ public class Person {
         private String surname;
         private String city;
         private String phoneNumber;
-        public Person (String name, String surname, String city, String phoneNumber) {
+
+
+        public Person(String name, String surname, String city, String phoneNumber) {
             this.name = name;
             this.surname = surname;
             this.city = city;
             this.phoneNumber = phoneNumber;
         }
-        public String personInfo() {
+        public static String personInfo(String name, String surname, String city, String phoneNumber) {
             return "Позвонить гражданину "
                     + name + " " + surname + " из города " + city + " можно по номеру " + phoneNumber + ".";
         }
 
         public static void main(String[] args) {
-            Person person1 = new Person("Will", "Smith", "Нью-Йорк", "2936729462846");
-            Person person2 = new Person("Jackie", "Chan", "Шанхай", "12312412412");
-            Person person3 = new Person("Sherlock", "Holmes", "London", "37742123513");
-            System.out.println(person1.personInfo());
-            System.out.println(person2.personInfo());
-            System.out.println(person3.personInfo());
+          String person1Info =  Person.personInfo ("Will", "Smith", "Нью-Йорк", "2936729462846");
+            String person2Info =  Person.personInfo ("Jackie", "Chan", "Шанхай", "12312412412");
+            String person3Info =  Person.personInfo("Sherlock", "Holmes", "London", "37742123513");
+            System.out.println(person1Info);
+            System.out.println(person2Info);
+            System.out.println(person3Info);
         }
     }
 
